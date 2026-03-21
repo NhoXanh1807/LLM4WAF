7d2c2af7c6996c677b21e2d31362cbe20599c193
akngg <nguyenanhkiet9052@gmail.com>
2026-03-21 11:48:24 +0700

    Update llm_service.py

--- src/gui/backend/services/llm_service.py
+++ src/gui/backend/services/llm_service.py
@@ -37,3 +37,17 @@ def chatgpt_completion(messages=[], model=None, response_format=None):
 
     response = requests.post(url, headers=headers, json=body)
     return response.json()
+
+def generate_response(prompt, model_name="GEMMA_2B", adapter_name="phase1", max_new_tokens=128, temperature=0.7):
+    try:
+        res = requests.post(f"http://api.akng.io.vn:89/generation?model_name={model_name}&adapter_name={adapter_name}&max_new_tokens={max_new_tokens}&temperature={temperature}", data=prompt)
+        if (
+            res.status_code == 200
+            and "data" in res.json()
+        ):
+            return res.json()["data"]
+        else:
+            return None
+    except Exception as e:
+        print(f"Error calling generation API: {e}")
+        return None
